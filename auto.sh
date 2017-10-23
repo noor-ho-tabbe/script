1914baf2fc108c072c5ed2e4742a1dc78e77fb91
noorhotabbe <noorhotabbe@163.com>
2017-10-23 01:58:26 -0700

    first commit

---
+++ auto.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+n=0
+while n<100
+do
+    ./phone_msg
+    sleep 6
+    kill -s 9 `ps -aux | grep phone_msg | grep -v grep `
+    if n%3 == 0
+        sleep 30
+    fi
+done
+
+#| awk '{print $2}'
