e311c8694e475fe1bccaf2658ab634bd22313075
栗山 <noorhotabbe@163.com>
2017-10-24 10:08:02 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,10 +4,8 @@ while n<100
 do
     ./phone_msg
     sleep 6
-    kill -s 9 `ps -aux | grep phone_msg | grep -v grep `
+    kill -s 9 `ps -aux | grep phone_msg | grep -v grep awk '{print $2}' `
     if n%3 == 0
         sleep 30
     fi
 done
-
-#| awk '{print $2}'
