070f5d6d02296f92ba5cca7b019c11ec0a7edaa0
noorhotabbe <noorhotabbe@163.com>
2017-10-25 18:55:31 -0700

    modify a little

--- auto.sh
+++ auto.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+# Loop start and kill a process
+
 n=1
-while (( $n < 100 ))
+while (( $n < 20 ))
 do
     echo "n=" $n
     tmp=$(($n % 3))
     sleep 2
-    # kill -s 9 `ps -aux | grep phone_msg | grep -v grep awk '{print $2}' `
+    kill -s 9 `ps -aux | grep phone_msg | grep -v grep | awk '{print $2}' `
     if [ $tmp -eq 0 ]; then
-        echo "test"
+        sleep 3
+        echo "Waiting for phone to exit !"
     fi
     ((n++))
 done
